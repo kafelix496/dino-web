76b5dfb74a1f300328a6649d7748161c89d9bb9c
Jiyeol Lee <41712713+kafelix496@users.noreply.github.com>
2021-12-10 08:02:13 -0500

    fix: header design (#3)

--- src/components/Layout/Header/PaletteModeButton/PaletteModeButton.tsx
+++ src/components/Layout/Header/PaletteModeButton/PaletteModeButton.tsx
@@ -1,48 +1,24 @@
 import { useDispatch } from 'react-redux'
 import { togglePaletteMode } from '@/redux-action-creators'
 
-import IconButton, { IconButtonProps } from '@mui/material/IconButton'
+import IconButton from '@mui/material/IconButton'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
-import { styled } from '@mui/material/styles'
 
 import type { FC } from 'react'
-
-const CustomizedIconButton = styled(
-  ({ className, ...props }: IconButtonProps) => (
-    <IconButton {...props} classes={{ root: className }} />
-  )
-)(
-  ({ theme }) => `
-  &.MuiIconButton-root {
-    border: 1px solid ${theme.palette.primary.main};
-    border-radius: 10px;
-  }
-  &.MuiIconButton-root:hover {
-    background-color: ${theme.palette.bgHoverDim};
-  }
-
-  &.MuiIconButton-root .MuiSvgIcon-root {
-    color: ${
-      theme.palette.mode === 'dark'
-        ? theme.palette.primary.contrastText
-        : theme.palette.primary.main
-    };
-  }
-`
-)
 
 const PaletteModeButton: FC = () => {
   const dispatch = useDispatch()
 
   return (
-    <CustomizedIconButton
+    <IconButton
       data-testid="palette-mode-button"
+      color="primary"
       onClick={() => {
         dispatch(togglePaletteMode())
       }}
     >
       <SettingsBrightnessIcon />
-    </CustomizedIconButton>
+    </IconButton>
   )
 }
 
