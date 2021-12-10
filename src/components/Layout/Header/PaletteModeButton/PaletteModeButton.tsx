636474e2a0e6380c23f5262222b3bb060b803987
Jiyeol Lee <ka.felix496@gmail.com>
2021-12-10 09:07:24 -0500

    fix: header-design

--- src/components/Layout/Header/PaletteModeButton/PaletteModeButton.tsx
+++ src/components/Layout/Header/PaletteModeButton/PaletteModeButton.tsx
@@ -4,15 +4,18 @@ import { togglePaletteMode } from '@/redux-action-creators'
 import IconButton from '@mui/material/IconButton'
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
 
+import { useHeaderButtonColor } from '../useHeaderButtonColor'
+
 import type { FC } from 'react'
 
 const PaletteModeButton: FC = () => {
   const dispatch = useDispatch()
+  const headerButtonColor = useHeaderButtonColor()
 
   return (
     <IconButton
       data-testid="palette-mode-button"
-      color="primary"
+      sx={{ color: headerButtonColor }}
       onClick={() => {
         dispatch(togglePaletteMode())
       }}
