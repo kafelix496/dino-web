b68547d3b3a6f4f0bf6c696f36df40befbb0af8a
Jiyeol Lee <ka.felix496@gmail.com>
2021-12-20 22:49:34 -0500

    fix: no more snapshot testing

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -3,10 +3,6 @@ import { Provider } from 'react-redux'
 import { render } from '@testing-library/react'
 import type { RenderOptions } from '@testing-library/react'
 import { makeStore } from '@/redux-store'
-import renderer from 'react-test-renderer'
-import type { ReactTestRendererJSON } from 'react-test-renderer'
-
-export type Snapshot = ReactTestRendererJSON | ReactTestRendererJSON[] | null
 
 const Providers: FC = ({ children }) => (
   <Provider store={makeStore()}>{children}</Provider>
@@ -17,9 +13,5 @@ const customRender = (
   options?: Omit<RenderOptions, 'wrapper'>
 ) => render(ui, { wrapper: Providers, ...options })
 
-const createSnapshot = (ui: ReactElement) =>
-  renderer.create(<Provider store={makeStore()}>{ui}</Provider>).toJSON()
-
 export * from '@testing-library/react'
-export { createSnapshot }
 export { customRender as render }
