b57a0ee08dd3e826444b2503a0962a1731100148
Jiyeol Lee <ka.felix496@gmail.com>
2022-01-16 19:07:19 -0500

    feat(issue-36): add amazon s3 connection functions

--- environment.d.ts
+++ environment.d.ts
@@ -1,12 +1,15 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NEXT_PUBLIC_APP_URL: string
+      AWS_BUCKET_NAME: string
+      AWS_BUCKET_REGION: string
+      AWS_ACCESS_KEY: string
+      AWS_SECRET_KEY: string
       DATABASE_URL: string
-      NEXTAUTH_URL: string
       GOOGLE_ID: string
       GOOGLE_SECRET: string
       JWT_SECRET: string
+      PAGE_URL: string
     }
   }
 }
