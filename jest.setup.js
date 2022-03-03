e8bdcdc2f643e57664b3b20caec42feaafe6e633
Jiyeol Lee <ka.felix496@gmail.com>
2022-03-02 20:07:56 -0500

    refactor: remove deprecated code

--- jest.setup.js
+++ jest.setup.js
@@ -1,11 +1,1 @@
 import '@testing-library/jest-dom/extend-expect'
-
-jest.mock('@/redux-actions', () => {
-  const originalModule = jest.requireActual('@/redux-actions')
-
-  return {
-    __esModule: true,
-    ...originalModule,
-    togglePaletteMode: jest.fn(() => ({ type: '' }))
-  }
-})
