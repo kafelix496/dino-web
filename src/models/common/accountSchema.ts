6ad6a0e517b7da42f8f22f373f7e309f91706242
Jiyeol Lee <41712713+kafelix496@users.noreply.github.com>
2022-04-28 07:35:09 -0400

    chore: remove version key (#94)

--- src/models/common/accountSchema.ts
+++ src/models/common/accountSchema.ts
@@ -2,36 +2,41 @@ import mongoose from 'mongoose'
 
 import { CollectionName } from '@/constants/collection'
 
-const accountSchema = new mongoose.Schema({
-  type: {
-    type: String
+const accountSchema = new mongoose.Schema(
+  {
+    type: {
+      type: String
+    },
+    provider: {
+      type: String
+    },
+    providerAccountId: {
+      type: String
+    },
+    access_token: {
+      type: String
+    },
+    expires_at: {
+      type: Number
+    },
+    token_type: {
+      type: String
+    },
+    scope: {
+      type: String
+    },
+    id_token: {
+      type: String
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: CollectionName.USER
+    }
   },
-  provider: {
-    type: String
-  },
-  providerAccountId: {
-    type: String
-  },
-  access_token: {
-    type: String
-  },
-  expires_at: {
-    type: Number
-  },
-  token_type: {
-    type: String
-  },
-  scope: {
-    type: String
-  },
-  id_token: {
-    type: String
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: CollectionName.USER
+  {
+    versionKey: false
   }
-})
+)
 
 accountSchema.set('timestamps', true)
 
--- src/models/common/sessionSchema.ts
+++ src/models/common/sessionSchema.ts
@@ -2,19 +2,24 @@ import mongoose from 'mongoose'
 
 import { CollectionName } from '@/constants/collection'
 
-const sessionSchema = new mongoose.Schema({
-  expires: {
-    type: Date
+const sessionSchema = new mongoose.Schema(
+  {
+    expires: {
+      type: Date
+    },
+    sessionToken: {
+      type: String,
+      unique: true
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: CollectionName.USER
+    }
   },
-  sessionToken: {
-    type: String,
-    unique: true
-  },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: CollectionName.USER
+  {
+    versionKey: false
   }
-})
+)
 
 sessionSchema.set('timestamps', true)
 
