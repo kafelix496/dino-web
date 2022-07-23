54866bbeb3d2886b58323dc31729b9de2d08e69e
Jiyeol Lee <ka.felix496@gmail.com>
2022-07-23 09:53:08 -0400

    add additional config for pwa

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const runtimeCaching = require('next-pwa/cache')
 module.exports = withPWA({
   pwa: {
     dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
     runtimeCaching
   },
   i18n,
