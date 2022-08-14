361c08f44cf1527b331df13fb7005007c0e85b0b
Jiyeol Lee <ka.felix496@gmail.com>
2022-08-14 17:30:26 -0400

    fix test

--- src/hooks/useTimestampText.test.ts
+++ src/hooks/useTimestampText.test.ts
@@ -3,21 +3,25 @@ import { renderHook } from '@/utils/testing-library'
 import { useCreatedAtText, useUpdatedAtText } from './useTimestampText'
 
 describe('#useCreatedAtText', () => {
-  it('should return false if the user is not logged in', () => {
+  it('should convert client timezone time with created at text', () => {
     const { result } = renderHook(() =>
       useCreatedAtText('2022-08-12T14:15:22.178Z')
     )
 
-    expect(result.current).toBe('CREATED_AT: 08/12/2022 10:15 am')
+    expect(result.current).toMatch(
+      /^CREATED_AT: \d\d\/\d\d\/\d\d\d\d \d\d:\d\d (a|p)m/
+    )
   })
 })
 
 describe('#useUpdatedAtText', () => {
-  it('should return false if the user is not logged in', () => {
+  it('should convert client timezone time with updated at text', () => {
     const { result } = renderHook(() =>
       useUpdatedAtText('2022-08-12T14:15:22.178Z')
     )
 
-    expect(result.current).toBe('UPDATED_AT: 08/12/2022 10:15 am')
+    expect(result.current).toMatch(
+      /^UPDATED_AT: \d\d\/\d\d\/\d\d\d\d \d\d:\d\d (a|p)m/
+    )
   })
 })
