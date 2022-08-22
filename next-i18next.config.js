2d4d87f8dbd2407b47210ed41cc3c08163f5eaa9
Jiyeol Lee <ka.felix496@gmail.com>
2022-08-22 00:17:27 -0400

    try to remove console error

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,9 +4,6 @@ module.exports = {
   i18n: {
     locales: ['default', 'en', 'kr'],
     defaultLocale: 'default',
-    localeDetection: false,
-    ...(typeof window === 'undefined'
-      ? { localePath: path.resolve('./public/locales') }
-      : {})
+    localeDetection: false
   }
 }
