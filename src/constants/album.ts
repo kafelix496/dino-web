8c3058c7745ccacbcb8afd2cc700d51414d0d1a6
Jiyeol Lee <ka.felix496@gmail.com>
2022-09-02 12:35:57 -0400

    undo temp code

--- src/constants/album.ts
+++ src/constants/album.ts
@@ -1,7 +1,7 @@
 export const POST_MAX_WIDTH = 600
 export const POST_ROW_HEIGHT = 250
 export const POST_MAX_ASSET_WIDTH = 1280
-export const POST_PAGE_SIZE = 1
+export const POST_PAGE_SIZE = 10
 
 export enum PostAssetTargets {
   SINGLE = 'SINGLE',
