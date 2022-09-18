2906815c1494872a5d7184f8ac0233f20130b5fc
Jiyeol Lee <ka.felix496@gmail.com>
2022-09-17 21:26:46 -0400

    add category chip click handler

--- src/components/album/PostListItemCategoryList/PostListItemCategoryList.tsx
+++ src/components/album/PostListItemCategoryList/PostListItemCategoryList.tsx
@@ -3,6 +3,7 @@ import type { FC } from 'react'
 import Chip from '@mui/material/Chip'
 import Stack from '@mui/material/Stack'
 
+import { usePostPageQueryParams } from '@/hooks/usePostPageQueryParams'
 import type { Category } from '@/types/album'
 
 interface PostListItemCategoryListProps {
@@ -12,10 +13,23 @@ interface PostListItemCategoryListProps {
 export const PostListItemCategoryList: FC<PostListItemCategoryListProps> = ({
   categories
 }) => {
+  const { patch } = usePostPageQueryParams()
+
+  const handleClick = (category: Category) => {
+    patch({ qpCategoryId: category._id })
+  }
+
   return (
     <Stack direction="row" spacing={1}>
       {categories.map((category) => (
-        <Chip key={category._id} label={category.name} size="small" />
+        <Chip
+          key={category._id}
+          label={category.name}
+          size="small"
+          onClick={() => {
+            handleClick(category)
+          }}
+        />
       ))}
     </Stack>
   )
